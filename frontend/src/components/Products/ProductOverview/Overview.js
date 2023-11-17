ba0f5bbd879d582a86194d171632648a64e1e9fa
muntiCloud <muntazacloud@gmail.com>
2023-11-16 17:47:46 -0800

    adding more style

--- frontend/src/components/Products/ProductOverview/Overview.js
+++ frontend/src/components/Products/ProductOverview/Overview.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '../../../assets/styles/components/products/Overview.module.css'
-// import Sidebar from '../../Sidebar/Sidebar'
 import ProductSidebar from './ProductSidebar'
 import ProductMain from './ProductMain'
 function Overview (){
